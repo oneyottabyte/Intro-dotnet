5d43e917d706b6f5bf6b37bc20a16206d6cd155e
oneyottabyte <junhinhodorian@gmail.com>
2021-01-24 22:39:58 -0300

    Finalizando aula 2

--- DigitalInovationOne/Program.cs
+++ DigitalInovationOne/Program.cs
@@ -6,7 +6,12 @@ namespace DigitalInovationOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                int numeroDeVezes = 10;
+
+                for (int i=0; i < numeroDeVezes; i++)
+                {
+                    Console.WriteLine($"Bem Vindo ao Curso de .NET {i}");
+                }
         }
     }
 }
